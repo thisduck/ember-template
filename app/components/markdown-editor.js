02f1b9fdc09583caa16aa1c3662550be7e7c47a0
Adnan Ali <adnan.ali@gmail.com>
2019-01-03 04:12:23 -0500

    Adding and hooking up Markdown
    Commands:
      - npm install remarkable

--- app/components/markdown-editor.js
+++ app/components/markdown-editor.js
@@ -1,13 +1,14 @@
 import Component, { tracked } from 'sparkles-component';
 import { htmlSafe } from '@ember/string';
+import Remarkable from 'remarkable';
+
+const md = new Remarkable();
 
 export default class MarkdownEditor extends Component {
   @tracked text = "# hey\nthere."
 
   @tracked('text')
   get html() {
-    return htmlSafe(this.text);
+    return htmlSafe(md.render(this.text));
   }
-
-
 }
